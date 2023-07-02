029873bfb05786a32be8153ad467ac0cef4aeda8
Ung Nguyen <commitment1511@outlook.com>
2023-07-02 20:00:21 +0700

    css pagination

--- lib/components/pagination.dart
+++ lib/components/pagination.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_traning/constants/color.dart';
 
 class PaginationBar extends StatelessWidget {
   final int currentPage;
@@ -16,9 +17,22 @@ class PaginationBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconButton(
-          icon: Icon(Icons.chevron_left),
-          onPressed: (currentPage > 1) ? () => onPageSelected(currentPage - 1) : null,
+        Container(
+          decoration: BoxDecoration(
+            //backgound color white
+            color: AppColors().secondaryBackgroundColor,
+            //shape circle
+            shape: BoxShape.circle,
+          ),
+          margin: EdgeInsets.only(right: 16),
+          child: IconButton(
+            //color blue
+            color: Colors.blue,
+            icon: Icon(Icons.chevron_left),
+            onPressed: (currentPage > 1)
+                ? () => onPageSelected(currentPage - 1)
+                : ()=>null,
+          ),
         ),
         if (currentPage > 3) ...[
           _buildPageButton(1),
@@ -33,30 +47,47 @@ class PaginationBar extends StatelessWidget {
           if (currentPage < totalPages - 3) _buildDots(),
           _buildPageButton(totalPages),
         ],
-        IconButton(
-          icon: Icon(Icons.chevron_right),
-          onPressed: (currentPage < totalPages) ? () => onPageSelected(currentPage + 1) : null,
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors().secondaryBackgroundColor,
+            shape: BoxShape.circle,
+          ),
+          margin: EdgeInsets.only(left: 16),
+          child: IconButton(
+            color: Colors.blue,
+            icon: Icon(Icons.chevron_right),
+            onPressed: (currentPage < totalPages)
+                ? () => onPageSelected(currentPage + 1)
+                : null,
+          ),
         ),
       ],
     );
   }
 
   Widget _buildPageButton(int page) {
-    return IconButton(
-      icon: Text(
-        page.toString(),
-        style: TextStyle(
-          color: (page == currentPage) ? Colors.blue : Colors.black,
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        color: (page == currentPage) ? Colors.blue : Colors.transparent,
+        shape: BoxShape.circle,
       ),
-      onPressed: () => onPageSelected(page),
+      child: IconButton(
+        icon: Text(
+          page.toString(),
+          style: TextStyle(
+            color: (AppColors().textPrimaryColor),
+          ),
+        ),
+        onPressed: () => onPageSelected(page),
+      ),
     );
   }
 
   Widget _buildDots() {
     return IconButton(
+      color: AppColors().textPrimaryColor,
       icon: Icon(Icons.more_horiz),
-      onPressed: null,
+      onPressed: ()=>null,
     );
   }
 }
