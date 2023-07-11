b33dbbc1e6e38bc35a2dfd950fa04ca005805a0c
ung nguyen <ung.nguyen@datx.com.vn>
2023-07-11 16:38:05 +0700

    use call handler

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_traning/utils/logger.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class Detail extends StatefulWidget {
   const Detail({super.key, required this.code});
@@ -18,18 +20,32 @@ class _DetailState extends State<Detail> {
     super.initState();
     controller = WebViewController()
       ..loadRequest(
-        Uri.parse('https://dev-tradingview.datxasia.com/recommendation/?symbol='+widget.code),
+        Uri.parse(
+            'https://dev-tradingview.datxasia.com/recommendation/?symbol=' +
+                widget.code),
       )
       //enable javascript
       ..setJavaScriptMode(JavaScriptMode.unrestricted);
   }
 
+  InAppWebViewController? webViewController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: WebViewWidget(
-          controller: controller,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(url: Uri.parse('http://10.0.2.2:3000/register-collaborator')),
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+            webViewController?.addJavaScriptHandler(
+              handlerName: 'backToApp',
+              callback: (a) {
+                // Handle the JavaScript call here
+                appLogger.e('backToApp');
+              },
+            );
+          },
         ),
       ),
     );
